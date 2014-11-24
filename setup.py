745a1bfccc90b40bf69837cff55e878c5e913d20
Francesco Levorato <git@flevour.net>
2014-11-24 15:26:50 +0100

    attempts

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup
 
+
 setup(
     name='idt_oauth2',
     version='0.1',
-    packages=['idt_oauth2'],
     package_dir={'idt_oauth2': '.'},
     author='Francesco Levorato',
     author_email='francesco@idonethis.com',
