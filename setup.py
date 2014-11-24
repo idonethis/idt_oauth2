3c3f7d6169b16aa76cd669d3bc6210857b9fae07
Francesco Levorato <git@flevour.net>
2014-11-24 15:32:40 +0100

    attempts

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 setup(
     name='idt_oauth2',
     version='0.1',
-    package_dir={'idt_oauth2': '.'},
+    packages=['idt_oauth2'],
     author='Francesco Levorato',
     author_email='francesco@idonethis.com',
     description='A simple django app supporting OAuth2 authentication flows',
