a219042cf931ddae0bfb8975f169afa3e443815a
Francesco Levorato <git@flevour.net>
2014-11-24 14:34:28 +0100

    fix integration with google

--- clients/google.py
+++ clients/google.py
@@ -1,5 +1,7 @@
 import logging
 import requests
+import base64
+import json
 
 from django.conf import settings
 
@@ -22,6 +24,7 @@ def token_exchange(code, redirect_uri):
         'client_id': settings.GOOGLE_OAUTH_CLIENT_ID,
         'client_secret': settings.GOOGLE_OAUTH_CLIENT_SECRET,
         'code': code,
+        'grant_type': 'authorization_code',
         'redirect_uri': redirect_uri,
     }
     logging.info(
@@ -32,4 +35,28 @@ def token_exchange(code, redirect_uri):
     logging.info(
             'google oauth2 token exchange response status code: %s, content: %s',
             response.status_code, response.json())
-    return response.json()
+    data = response.json()
+
+    user_data = _decode_id_token(data['id_token'])
+    data['user_data'] = user_data
+    data['email'] = user_data['email']
+
+    return data
+
+
+def _decode_id_token(id_token):
+    _, payload, _ = id_token.split('.')
+    return json.loads(_decode_base64(payload))
+
+
+def _decode_base64(data):
+    """Decode base64, padding being optional.
+
+    :param data: Base64 data as an ASCII byte string
+    :returns: The decoded byte string.
+
+    """
+    missing_padding = 4 - len(data) % 4
+    if missing_padding:
+        data += b'='* missing_padding
+    return base64.decodestring(data)
